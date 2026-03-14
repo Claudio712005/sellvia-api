35185296bf84b9c842fbb390b65f5706b545d0cd
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-03-14 14:10:13 -0300

    Ajuste dockerfile e gradlew para corrigir versão com o banco de dados

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     kotlin("plugin.allopen") version "1.9.25"
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
-    id("org.flywaydb.flyway") version "10.17.0"
+    id("org.flywaydb.flyway") version "11.3.2"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 
     jacoco
@@ -60,12 +60,6 @@ dependencyManagement {
     }
 }
 
-buildscript {
-    dependencies {
-        classpath("org.flywaydb:flyway-database-postgresql:10.17.0")
-    }
-}
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -73,7 +67,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
-    implementation("org.flywaydb:flyway-core")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -91,7 +84,13 @@ dependencies {
     testImplementation("io.mockk:mockk:1.14.9")
     testImplementation("io.kotest:kotest-assertions-core:5.8.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.flywaydb:flyway-core:11.3.2")
+    implementation("org.flywaydb:flyway-database-postgresql:11.3.2")
+
 }
+
+extra["flyway.version"] = "11.3.2"
 
 kotlin {
     compilerOptions {
@@ -108,6 +107,14 @@ apply(from = "jacoco-report.gradle.kts")
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.flywaydb") {
+            useVersion("11.3.2")
+        }
+    }
 }
 
 val jacocoScript = rootProject.file("jacoco-report.gradle.kts")
